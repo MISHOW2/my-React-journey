04389d94b0c504e1189dda0e9cdc69e69b586be4
MISHOW2 <mshibambo63@gmail.com>
2024-10-08 22:23:04 +0200

    changed nothing

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,35 +1,134 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [formData, setFormData] = useState(
+    {
+        firstName: "", 
+        lastName: "", 
+        email: "", 
+        comments: "", 
+        isFriendly: true,
+        employment: "",
+        favColor: ""
+    }
+)
+
+function handleChange(event) {
+    const {name, value, type, checked} = event.target
+    setFormData(prevFormData => {
+        return {
+            ...prevFormData,
+            [name]: type === "checkbox" ? checked : value
+        }
+    })
 }
 
-export default App
+function handleSubmit(event) {
+    event.preventDefault()
+    // submitToApi(formData)
+    console.log(formData)
+}
+
+return (
+    <form onSubmit={handleSubmit}>
+        <input
+            type="text"
+            placeholder="First Name"
+            onChange={handleChange}
+            name="firstName"
+            value={formData.firstName}
+        />
+        <input
+            type="text"
+            placeholder="Last Name"
+            onChange={handleChange}
+            name="lastName"
+            value={formData.lastName}
+        />
+        <input
+            type="email"
+            placeholder="Email"
+            onChange={handleChange}
+            name="email"
+            value={formData.email}
+        />
+        <textarea 
+            value={formData.comments}
+            placeholder="Comments"
+            onChange={handleChange}
+            name="comments"
+        />
+        <input 
+            type="checkbox" 
+            id="isFriendly" 
+            checked={formData.isFriendly}
+            onChange={handleChange}
+            name="isFriendly"
+        />
+        <label htmlFor="isFriendly">Are you friendly?</label>
+        <br />
+        <br />
+        
+        <fieldset>
+            <legend>Current employment status</legend>
+            <input 
+                type="radio"
+                id="unemployed"
+                name="employment"
+                value="unemployed"
+                checked={formData.employment === "unemployed"}
+                onChange={handleChange}
+            />
+            <label htmlFor="unemployed">Unemployed</label>
+            <br />
+            
+            <input 
+                type="radio"
+                id="part-time"
+                name="employment"
+                value="part-time"
+                checked={formData.employment === "part-time"}
+                onChange={handleChange}
+            />
+            <label htmlFor="part-time">Part-time</label>
+            <br />
+            
+            <input 
+                type="radio"
+                id="full-time"
+                name="employment"
+                value="full-time"
+                checked={formData.employment === "full-time"}
+                onChange={handleChange}
+            />
+            <label htmlFor="full-time">Full-time</label>
+            <br />
+        </fieldset>
+        <br />
+        
+        <label htmlFor="favColor">What is your favorite color?</label>
+        <br />
+        <select 
+            id="favColor" 
+            value={formData.favColor}
+            onChange={handleChange}
+            name="favColor"
+        >
+            <option value="red">Red</option>
+            <option value="orange">Orange</option>
+            <option value="yellow">Yellow</option>
+            <option value="green">Green</option>
+            <option value="blue">Blue</option>
+            <option value="indigo">Indigo</option>
+            <option value="violet">Violet</option>
+        </select>
+        <br />
+        <br />
+        <button>Submit</button>
+    </form>
+)
+}
+
--- renderJoke/src/Joke.jsx
+++ renderJoke/src/Joke.jsx
@@ -7,16 +7,28 @@ export default function Joke(props) {
      * - Add a button that toggles the value back and forth
      */
     const [isShown, setShown] = useState(false);
+     const btnMessage = ""
       function toggle() {
-        setShown(isShown=>!isShown)
+       
+        setShown(isShown=>{
+          return (!isShown)
+        })
+      
       }
 
     return (
         <div> 
        
-            {props.setup && <h3>{props.setup}</h3>}
-            {!isShown && <p>{props.punchline}</p> }
-            <button onClick={toggle}>Show punchline</button>
+            {
+            props.setup && <h3>{props.setup}</h3>
+            
+            }
+            {
+            !isShown && <p>{props.punchline}</p> 
+            
+            }
+
+            <button onClick={toggle}>{isShown?"Hide":"Show"} Punchline</button>
             <hr />
         </div>
     )
